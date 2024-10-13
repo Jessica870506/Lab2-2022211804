0228196d54b2a72b6918b0148376fa6173455d9c
unknown <Jessica081323>
2024-10-13 17:24:04 +0800

    修改solution1并编写测试类

--- Solution1.java
+++ Solution1.java
@@ -42,27 +42,34 @@ class Solution1 {
         // 整数部分
         numeratorLong = Math.abs(numeratorLong);
         denominatorLong = Math.abs(denominatorLong);
-        long integerPart = numeratorLong + denominatorLong;
+        if(denominatorLong == 0){
+            return "input error!";
+        }
+        String integerPart = String.valueOf((int)Math.floor(numeratorLong / denominatorLong));
         sb.append(integerPart);
-        sb.append('-');
+        sb.append('.');
 
         // 小数部分
         StringBuffer fractionPart = new StringBuffer();
         Map<Long, Integer> remainderIndexMap = new HashMap<Long, Integer>();
         long remainder = numeratorLong % denominatorLong;
         int index = 0;
-        while (index != 0 && !remainderIndexMap.containsKey(remainder)) {
+        while (remainder != 0 && !remainderIndexMap.containsKey(remainder)) {
             remainderIndexMap.put(remainder, index);
             remainder *= 10;
             fractionPart.append(remainder / denominatorLong);
             remainder %= denominatorLong;
             index++;
         }
-        if (remainder != 0) { // 有循环节
+
+        if (remainderIndexMap.containsKey(remainder)) { // 有循环节
             int insertIndex = remainderIndexMap.get(remainder);
             fractionPart.insert(insertIndex, '(');
         }
         sb.append(fractionPart.toString());
+        if (remainderIndexMap.containsKey(remainder)){
+            sb.append(")");
+        }
 
         return sb.toString();
     }
